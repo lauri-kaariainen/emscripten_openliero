9b55c579e5cbcf6fe6c69251bae1d2586ffa10f0
lauri <lauri@lauri.lauri>
2015-01-28 16:54:38 +0200

    deleted else but files for playing

--- compileO3.sh
+++
@@ -1,6 +0,0 @@
-cd liero;
-make;
-cp openliero ../openliero.bc;
-cd ..;
-emcc openliero.bc -O3 -o openliero.html --preload-file LEVEDIT.EXE --preload-file LICENSE.TXT --preload-file LIERO.CHR --preload-file LIERO.DAT --preload-file LIEROENG.TXT --preload-file LIERO.EXE --preload-file LIERO.OPT --preload-file LIERO.SND --preload-file LIERO.TXT --preload-file NAMES.DAT --preload-file WINXP.BAT --preload-file FILE_ID.DIZ --preload-file LIERO.dat --preload-file Profiles/ --preload-file STBANA2.LEV --preload-file  STBANA2.bmp
-
